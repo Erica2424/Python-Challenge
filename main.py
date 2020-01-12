66d31047d7185b167a3e6bf10fc30381065d3904
Erica Matrese <ericamatrese@Ericas-MacBook-Pro.local>
2020-01-12 13:26:53 -0500

    homework

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import os
 import csv
-budget_csv = '/Users/ericamatrese/Desktop/GWU-ARL-DATA-PT-12-2019-U-C/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv'
+budget_csv = os.path.join("Resources", "budget_data.csv")
+
+#total months
+months = 0
+#net total profit/losses
+total = 0
+#change in average
+change= 0
+change_list = []
+with open(budget_csv) as data:
+    csv_reader= csv.reader(data)
+    header=next(csv_reader)
+    first_row= next(csv_reader)
+    total = total+int(first_row[1])
+    months+=1
+    previous_row= int(first_row[1])
+    for i in csv_reader:
+        months+=1
+        total = total+int(i[1])
+        change= int(i[1])- previous_row
+        previous_row = int(i[1])
+        change_list.append(change)
+        print(i)
+        print(months)
+        print(total)
+average= sum(change_list)/len(change_list)
+print(average)
